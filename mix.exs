09ce90d46df2423c98a480438b713e8267eccda3
Shigeaki Matsumura <matsu911@gmail.com>
2023-01-16 21:24:43 +0900

    phoenix_live_view ~> 0.18.0

--- mix.exs
+++ mix.exs
@@ -74,7 +74,7 @@ defmodule LiveMap.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:phoenix_live_view, "~> 0.17.5"},
+      {:phoenix_live_view, "~> 0.18.0"},
       {:jason, "~> 1.2"},
 
       # Test/dev deps
